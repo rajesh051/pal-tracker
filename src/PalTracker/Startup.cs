8b5b32531c879264c0c303c21e8b6c59793874e9
rajesh051 <rajesh.051@wipro.com>
2019-11-07 11:22:39 +0530

    Health Monitor 1.0

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
 
 namespace PalTracker
 {
@@ -26,6 +30,8 @@ namespace PalTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
+            services.AddCloudFoundryActuators(Configuration);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton(sp => new WelcomeMessage(Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")));
             services.AddSingleton(sp => new CloudFoundryInfo(
@@ -36,6 +42,8 @@ namespace PalTracker
                 ));
                 services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
                 services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+                services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+                 services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +60,7 @@ namespace PalTracker
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseCloudFoundryActuators();
         }
     }
 }
